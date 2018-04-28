3b9e101902b52862320d9103b4d63c4547114565
Carlos Feria <carlosthe19916@gmail.com>
2018-04-28 13:03:01 -0500

    first commit

---
+++ release.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+echo "===================================="
+read -p "Enter Release: " releaseVersion
+read -p "Enter Next Release (SNAPSHOT): " nextVersion
+read -p "Enter Github Username: " username
+read -s -p "Enter Github Password: " password
+
+# Update to release
+mvn versions:set -DnewVersion="$releaseVersion"
+
+git add .
+git commit -m "Prepare release $releaseVersion"
+git push "https://$username:$password@github.com/carlosthe19916/sunat-web-services.git"
+
+# Create tag and push
+git tag "$releaseVersion"
+git push "https://$username:$password@github.com/carlosthe19916/sunat-web-services.git" --tags
+
+# Create next snapshot
+mvn versions:set -DnewVersion="$nextVersion-SNAPSHOT"
+
+git add .
+git commit -m "Prepare next release $nextVersion-SNAPSHOT"
+git push "https://$username:$password@github.com/carlosthe19916/sunat-web-services.git"

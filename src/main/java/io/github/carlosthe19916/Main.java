1be90070d26238ddd2e71d6b1fc57eba98f8c67f
Carlos Feria <carlosthe19916@gmail.com>
2018-05-16 16:20:06 -0500

    fix(custom package): move every wsdl to their own packages

--- src/main/java/io/github/carlosthe19916/Main.java
+++ src/main/java/io/github/carlosthe19916/Main.java
@@ -1,10 +1,9 @@
 package io.github.carlosthe19916;
 
-import pe.gob.sunat.servicio.registro.comppago.factura.gem.service.BillService;
-
 public class Main {
 
     public static void main(String[] args) {
-        BillService billService = new BillService();
+
     }
+
 }

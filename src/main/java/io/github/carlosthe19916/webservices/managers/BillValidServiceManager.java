80abd346f4b453af1dd631774a68348d795e0a9f
Carlos Feria <carlosthe19916@gmail.com>
2018-07-13 12:12:23 -0500

    feat(xml check): validation on xml files

--- src/main/java/io/github/carlosthe19916/webservices/managers/BillValidServiceManager.java
+++ src/main/java/io/github/carlosthe19916/webservices/managers/BillValidServiceManager.java
@@ -2,7 +2,6 @@ package io.github.carlosthe19916.webservices.managers;
 
 import io.github.carlosthe19916.webservices.wrappers.BillValidServiceWrapper;
 import io.github.carlosthe19916.webservices.wrappers.ServiceConfig;
-import service.sunat.gob.pe.billvalidservice.StatusResponse;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,19 +15,31 @@ public class BillValidServiceManager {
         // Just static methods
     }
 
-    public static StatusResponse getStatus(File file, ServiceConfig config) throws IOException {
+    public static service.sunat.gob.pe.billvalidservice.StatusResponse getStatus(File file, ServiceConfig config) throws IOException {
         return getStatus(file.toPath(), config);
     }
 
-    public static StatusResponse getStatus(Path path, ServiceConfig config) throws IOException {
+    public static service.sunat.gob.pe.billvalidservice.StatusResponse getStatus(Path path, ServiceConfig config) throws IOException {
         return getStatus(path.getFileName().toString(), Files.readAllBytes(path), config);
     }
 
-    public static StatusResponse getStatus(String fileName, byte[] file, ServiceConfig config) throws IOException {
+    public static service.sunat.gob.pe.billvalidservice.StatusResponse getStatus(String fileName, byte[] file, ServiceConfig config) throws IOException {
         byte[] encode = Base64.getEncoder().encode(file);
         String base64Encoded = new String(encode);
 
         return BillValidServiceWrapper.getStatus(config, fileName, base64Encoded);
     }
 
+    public static service.sunat.gob.pe.billvalidservice.StatusResponse validaCDPcriterios(BillValidBean bean, ServiceConfig config) {
+        return BillValidServiceWrapper.validaCDPcriterios(config,
+                bean.getRucEmisor(),
+                bean.getTipoCDP(),
+                bean.getSerieCDP(),
+                bean.getNumeroCDP(),
+                bean.getTipoDocIdReceptor(),
+                bean.getNumeroDocIdReceptor(),
+                bean.getFechaEmision(),
+                bean.getImporteTotal(),
+                bean.getNroAutorizacion());
+    }
 }

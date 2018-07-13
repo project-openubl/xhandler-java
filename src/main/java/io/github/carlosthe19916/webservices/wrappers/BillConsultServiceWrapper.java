8a92b25decc0f5597c6e50ecfd6aa8babf6f3943
Carlos Feria <carlosthe19916@gmail.com>
2018-07-13 10:30:43 -0500

    feat(test bille consult): add basic test

--- src/main/java/io/github/carlosthe19916/webservices/wrappers/BillConsultServiceWrapper.java
+++ src/main/java/io/github/carlosthe19916/webservices/wrappers/BillConsultServiceWrapper.java
@@ -15,7 +15,7 @@ public class BillConsultServiceWrapper {
 
     public static service.sunat.gob.pe.billconsultservice.StatusResponse getStatusCdr(ServiceConfig config, String ruc, String tipo, String serie, int numero) {
         service.sunat.gob.pe.billconsultservice.BillService billService = ServiceFactory.getInstance(service.sunat.gob.pe.billconsultservice.BillService.class, config);
-        return billService.getStatus(ruc, tipo, serie, numero);
+        return billService.getStatusCdr(ruc, tipo, serie, numero);
     }
 
 }

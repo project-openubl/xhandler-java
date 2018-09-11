d6aad0436810a5dbf2d6d6108b0689f0a82debac
Carlos Feria <carlosthe19916@gmail.com>
2018-09-11 16:42:38 -0500

    fix(code Smell): code Smell

--- src/main/java/io/github/carlosthe19916/webservices/wrappers/BillServiceWrapper.java
+++ src/main/java/io/github/carlosthe19916/webservices/wrappers/BillServiceWrapper.java
@@ -1,6 +1,7 @@
 package io.github.carlosthe19916.webservices.wrappers;
 
 import io.github.carlosthe19916.webservices.factories.ServiceFactory;
+import io.github.carlosthe19916.webservices.models.types.InternetMediaType;
 
 import javax.activation.DataHandler;
 import javax.activation.DataSource;
@@ -14,7 +15,7 @@ public class BillServiceWrapper {
 
     public static byte[] sendBill(String fileName, byte[] zipFile, String partyType, ServiceConfig config) {
         service.sunat.gob.pe.billservice.BillService billService = ServiceFactory.getInstance(service.sunat.gob.pe.billservice.BillService.class, config);
-        DataSource dataSource = new ByteArrayDataSource(zipFile, "application/zip");
+        DataSource dataSource = new ByteArrayDataSource(zipFile, InternetMediaType.ZIP.getMimeType());
         DataHandler dataHandler = new DataHandler(dataSource);
         return billService.sendBill(fileName, dataHandler, partyType);
     }
@@ -26,14 +27,14 @@ public class BillServiceWrapper {
 
     public static String sendSummary(String fileName, byte[] zipFile, String partyType, ServiceConfig config) {
         service.sunat.gob.pe.billservice.BillService billService = ServiceFactory.getInstance(service.sunat.gob.pe.billservice.BillService.class, config);
-        DataSource dataSource = new ByteArrayDataSource(zipFile, "application/zip");
+        DataSource dataSource = new ByteArrayDataSource(zipFile, InternetMediaType.ZIP.getMimeType());
         DataHandler dataHandler = new DataHandler(dataSource);
         return billService.sendSummary(fileName, dataHandler, partyType);
     }
 
     public static String sendPack(String fileName, byte[] zipFile, String partyType, ServiceConfig config) {
         service.sunat.gob.pe.billservice.BillService billService = ServiceFactory.getInstance(service.sunat.gob.pe.billservice.BillService.class, config);
-        DataSource dataSource = new ByteArrayDataSource(zipFile, "application/zip");
+        DataSource dataSource = new ByteArrayDataSource(zipFile, InternetMediaType.ZIP.getMimeType());
         DataHandler dataHandler = new DataHandler(dataSource);
         return billService.sendPack(fileName, dataHandler, partyType);
     }

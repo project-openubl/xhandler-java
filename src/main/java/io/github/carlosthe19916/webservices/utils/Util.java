dcc4591381deba06f00802d1c73f08f88452807a
Carlos Feria <carlosthe19916@gmail.com>
2018-09-22 12:37:44 -0500

    feat(util): add util methods

--- src/main/java/io/github/carlosthe19916/webservices/utils/Util.java
+++ src/main/java/io/github/carlosthe19916/webservices/utils/Util.java
@@ -1,8 +1,19 @@
 package io.github.carlosthe19916.webservices.utils;
 
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.soap.SOAPFault;
 import javax.xml.ws.soap.SOAPFaultException;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.Optional;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class Util {
 
@@ -41,6 +52,35 @@ public class Util {
             return fileName.substring(0, fileName.lastIndexOf('.'));
         }
         return fileName;
+    }
+
+    public static byte[] getFirstXmlFileFromZip(byte[] data) throws IOException {
+        try (
+                ZipInputStream zis = new ZipInputStream(new ByteArrayInputStream(data));
+                ByteArrayOutputStream os = new ByteArrayOutputStream()
+        ) {
+
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                if (entry.getName().toLowerCase().endsWith(".xml")) {
+                    byte[] buffer = new byte[1024];
+                    int len;
+                    while ((len = zis.read(buffer)) > 0) {
+                        os.write(buffer, 0, len);
+                    }
+                    return os.toByteArray();
+                }
+            }
+
+        }
+        return null;
+    }
+
+    public static Document getDocumentFromBytes(byte[] cdrXml) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(true);
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        return builder.parse(new ByteArrayInputStream(cdrXml));
     }
 
 }
--- src/test/java/io/github/carlosthe19916/webservices/utils/UtilTest.java
+++ src/test/java/io/github/carlosthe19916/webservices/utils/UtilTest.java
@@ -2,22 +2,31 @@ package io.github.carlosthe19916.webservices.utils;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.mockito.Mockito;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.soap.SOAPFault;
 import javax.xml.ws.soap.SOAPFaultException;
-
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Optional;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class UtilTest {
 
+    /**
+     * Should get the correct error code from javax.xml.ws.soap.SOAPFaultException
+     * {@link io.github.carlosthe19916.webservices.utils.Util#getErrorCode(SOAPFaultException)}
+     */
     @Test
-    public void getErrorCode() {
+    public void test_shouldExtractErrorCode() {
         // Fault code: 100
         SOAPFault mockFault = mock(SOAPFault.class);
         when(mockFault.getFaultCode()).thenReturn("100");
@@ -64,12 +73,45 @@ public class UtilTest {
         Assert.assertEquals(100, (int) result.get());
     }
 
+    /**
+     * Should remove extension from fileName
+     * {@link io.github.carlosthe19916.webservices.utils.Util#getFileNameWithoutExtension(String)}
+     */
     @Test
-    public void getFileNameWithoutExtension() {
+    public void test_getFileNameWithoutExtension() {
         Assert.assertEquals("file", Util.getFileNameWithoutExtension("file"));
         Assert.assertEquals("file", Util.getFileNameWithoutExtension("file."));
         Assert.assertEquals("file", Util.getFileNameWithoutExtension("file.xml"));
         Assert.assertEquals("file.md", Util.getFileNameWithoutExtension("file.md.xml"));
     }
 
+    /**
+     * Should obtain the first XML file found on zip file
+     * {@link io.github.carlosthe19916.webservices.utils.Util#getFirstXmlFileFromZip(byte[])}
+     */
+    @Test
+    public void test_shouldReturnFirstXmlFound() throws URISyntaxException, IOException {
+        final File cdrZipFile = Paths.get(getClass().getResource("/ubl/cdr/cdr.zip").toURI()).toFile();
+        final byte[] cdrZipBytes = Files.readAllBytes(cdrZipFile.toPath());
+
+        final File cdrFile = Paths.get(getClass().getResource("/ubl/cdr/R-12345678901-01-F001-00000587.xml").toURI()).toFile();
+        final byte[] cdrBytes = Files.readAllBytes(cdrFile.toPath());
+
+        final byte[] cdrExtractedBytes = Util.getFirstXmlFileFromZip(cdrZipBytes);
+
+        Assert.assertTrue(Arrays.equals(cdrBytes, cdrExtractedBytes));
+    }
+
+    /**
+     * Should parse byte[] to xml document
+     * {@link io.github.carlosthe19916.webservices.utils.Util#getDocumentFromBytes(byte[])}
+     */
+    @Test
+    public void getDocumentFromBytes() throws URISyntaxException, IOException, ParserConfigurationException, SAXException {
+        final File file = Paths.get(getClass().getResource("/ubl/F001-00005954.xml").toURI()).toFile();
+        final byte[] bytes = Files.readAllBytes(file.toPath());
+
+        Document document = Util.getDocumentFromBytes(bytes);
+        Assert.assertNotNull(document);
+    }
 }

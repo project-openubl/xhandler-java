9402961507b12bb36850a59c3ee153ac0876faa5
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-04-19 13:46:34 +0200

    change license

--- cd/before-deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_22d558de97b8_key -iv $encrypted_22d558de97b8_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
-    gpg --fast-import cd/codesigning.asc
-fi
--- src/main/java/io/github/projectopenubl/xmlsenderlib/webservices/exceptions/UnknownWebServiceException.java
+++ src/main/java/io/github/projectopenubl/xmlsenderlib/webservices/exceptions/UnknownWebServiceException.java
@@ -1,12 +1,12 @@
-/*
- * Copyright 2017 Carlosthe19916, Inc. and/or its affiliates
+/**
+ * Copyright 2019 Project OpenUBL, Inc. and/or its affiliates
  * and other contributors as indicated by the @author tags.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Eclipse Public License - v 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.eclipse.org/legal/epl-2.0/
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.github.projectopenubl.xmlsenderlib.webservices.exceptions;
 
 import javax.xml.ws.soap.SOAPFaultException;

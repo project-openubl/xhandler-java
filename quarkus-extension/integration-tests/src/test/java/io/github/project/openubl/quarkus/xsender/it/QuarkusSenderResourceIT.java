7ed750c43600395c6d738fa7a38178ff962e9acf
carlosthe19916 <2582866+carlosthe19916@users.noreply.github.com>
2022-12-03 18:30:06 +0100

    Format code using IntelliJ style

--- quarkus-extension/integration-tests/src/test/java/io/github/project/openubl/quarkus/xsender/it/QuarkusSenderResourceIT.java
+++ quarkus-extension/integration-tests/src/test/java/io/github/project/openubl/quarkus/xsender/it/QuarkusSenderResourceIT.java
@@ -19,4 +19,5 @@ package io.github.project.openubl.quarkus.xsender.it;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
-public class QuarkusSenderResourceIT extends QuarkusXSenderResourceTest {}
+public class QuarkusSenderResourceIT extends QuarkusXSenderResourceTest {
+}
